840cbd99fd431111d7950c93c6b42113e0fa7540
ivanbermudez12 <ivan.bermudez01@estudiant.upf.edu>
2025-05-03 19:00:24 +0200

    test

--- test/test.c
+++ test/test.c
@@ -1,11 +1,19 @@
 #include "sample_lib_test.c"
 #include "utils.h"
+#include "document_parcing.c"
+#include "document_linked_list.c"
+#include "link_linked_list.c"
+#include "test_query.c"
 #include <stdio.h>
 
 int main() {
   {
     // Call all test modules you want to run here
     sample_lib_test();
+    test_document_desserialize();
+    test_document_list();
+    test_links_list();
+    test_query();
   }
   allsuccess();
 }
---
+++ test/test_query.c
@@ -0,0 +1,55 @@
+#include "document.h"
+#include "query.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void t1_query() {
+    runningtest("t1_query");
+    {
+        QueryNode *q = query_from_string("gato perro");
+        assert(q != NULL);
+        assert(strcmp(q->keyword, "gato") == 0);
+        assert(strcmp(q->next->keyword, "perro") == 0);
+        free_query(q);
+    }
+    successtest();  
+}
+
+void t2_query() {
+    runningtest("t2_query");
+    {
+        QueryNode *q = query_from_string("gato -perro");
+        Document d = {.body = "el gato duerme"};
+        assert(match_document(&d, q) == true);
+        d.body = "el gato y el perro";
+        assert(match_document(&d, q) == false);
+        free_query(q);
+    }
+    successtest();  
+}
+
+void t3_query() {
+    runningtest("t3_query");
+    {
+        QueryNode *q = query_from_string("gato | perro");
+        Document d = {.body = "el perro ladra"};
+        assert(match_document(&d, q) == true);
+        d.body = "el ratón";
+        assert(match_document(&d, q) == false);
+        free_query(q);
+    }
+    successtest();  
+}
+
+void test_query() {
+    running("test_query");
+    {
+      // Call all tests you want to run here
+      t1_query();
+      t2_query();
+      t3_query();
+    }
+    success();
+}
