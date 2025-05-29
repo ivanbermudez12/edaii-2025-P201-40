f9562e05219d1bb8a6cf015afd1118f427590484
Paula <paula.carrasco01@estudiant.upf.edu>
2025-05-29 15:58:53 +0200

    version correcta

--- src/query.h
+++ src/query.h
@@ -27,11 +27,6 @@ Query *query_from_string(const char *str);
 QueryNode *parse_query_nodes(const char *query_string);
 bool match_document(Document *doc, QueryNode *query);
 void free_query(Query *query);
-<<<<<<< HEAD
-=======
-
-struct DocumentGraph;
->>>>>>> f304e36a4fa4191ba61f7652e964c80c6ced772e
 Document *sort_by_relevance(Document *results, DocumentGraph *graph);
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,16 +1,10 @@
 #include "document_linked_list.c"
 #include "document_parcing.c"
 #include "link_linked_list.c"
-<<<<<<< HEAD
 #include "busqueda_linear_test.c"
 #include "test_query.c"
 #include "test_hashmap.c"
 #include "test_busqueda_hashmap.c"
-=======
-#include "sample_lib_test.c"
-#include "test_query.c"
-#include "utils.h"
->>>>>>> f304e36a4fa4191ba61f7652e964c80c6ced772e
 #include <stdio.h>
 
 int main() {
